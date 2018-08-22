67cd332d2ad2d2f0dca1e662f1ad9fa3ad3fe5eb
Christian Zöller <noex_home@yahoo.de>
2018-08-22 11:13:42 +0200

    Revealed the Dollar side-effects in the test.

--- src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
+++ src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
@@ -11,6 +11,8 @@ public class MoneyExampleTest {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
+        five.times(3);
+        assertEquals(15, five.amount);
     }
 
 }
