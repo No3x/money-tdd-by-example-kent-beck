0fd162759bfc04432c9e679f2b0fdc1b334687cf
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 12:41:12 +0200

    Added testCurrency().

--- src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
+++ src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
@@ -31,4 +31,9 @@ public class MoneyExampleTest {
         assertEquals(Money.franc(15), five.times(3));
     }
 
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
 }
