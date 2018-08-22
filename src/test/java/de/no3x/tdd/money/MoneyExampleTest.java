fb1518ed4766c9628d6a07ad8815fe1465ed23bd
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 15:41:21 +0200

    Added testPlusSameCurrencyReturnsMoney().

--- src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
+++ src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
@@ -104,4 +104,9 @@ public class MoneyExampleTest {
         assertEquals(Money.dollar(20), result);
     }
 
+    @Test
+    public void testPlusSameCurrencyReturnsMoney() {
+        Expression sum = Money.dollar(1).plus(Money.dollar(1));
+        assertTrue(sum instanceof Money);
+    }
 }
