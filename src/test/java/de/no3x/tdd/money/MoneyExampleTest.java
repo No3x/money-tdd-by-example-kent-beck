7d735f17e3bba7f185acd844021aa15f31c14c1e
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 14:07:01 +0200

    Added testReduceSum() to break the existing test.

--- src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
+++ src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
@@ -43,4 +43,12 @@ public class MoneyExampleTest {
         assertEquals(five, sum.augend);
         assertEquals(five, sum.addend);
     }
+
+    @Test
+    public void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), result);
+    }
 }
