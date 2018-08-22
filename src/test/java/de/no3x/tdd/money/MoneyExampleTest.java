42af60fa097f768f7a87c99f9299127303ffae72
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 14:51:10 +0200

    Added testIdentityRate() for checking the rate from USD to USD.

--- src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
+++ src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
@@ -66,4 +66,9 @@ public class MoneyExampleTest {
         Money result = bank.reduce(Money.franc(2), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
+    }
 }
