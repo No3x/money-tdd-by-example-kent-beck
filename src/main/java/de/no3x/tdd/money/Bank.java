69c6d2fdd9ae38c4ce453b22855078d2cfa35791
Christian Zöller <noex_home@yahoo.de>
2018-08-22 14:18:20 +0200

    Updated Bank.reduce() to return Money directly if the input is a Money. Ugly.

--- src/main/java/de/no3x/tdd/money/Bank.java
+++ src/main/java/de/no3x/tdd/money/Bank.java
@@ -3,6 +3,7 @@ package de.no3x.tdd.money;
 public class Bank {
 
     public Money reduce(Expression source, String to) {
+        if( source instanceof Money) return (Money) source;
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }
