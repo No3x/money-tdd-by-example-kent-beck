37dc9c263430c41e1a8095a3ec23b000e558e908
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 13:33:10 +0200

    Deleted the duplicated testFrancMultiplication().

--- src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
+++ src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
@@ -23,13 +23,6 @@ public class MoneyExampleTest {
     }
 
     @Test
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
