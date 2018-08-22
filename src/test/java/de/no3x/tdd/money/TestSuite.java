04fb90e471fc0283237a983800953b60148632f6
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 10:29:01 +0200

    Added simple test to verify testframework is working

---
+++ src/test/java/de/no3x/tdd/money/TestSuite.java
@@ -0,0 +1,14 @@
+package de.no3x.tdd.money;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestSuite {
+
+    @Test
+    public void verifyTestframeworkRuns() {
+        assertTrue(true);
+    }
+
+}
