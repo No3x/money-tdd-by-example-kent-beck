934035faa47066627edc70c8225cee3c59f9f457
Christian Zöller <noex_home@yahoo.de>
2018-08-22 12:53:59 +0200

    Fixed the two callers of Franc's constructor.

--- src/main/java/de/no3x/tdd/money/Money.java
+++ src/main/java/de/no3x/tdd/money/Money.java
@@ -16,7 +16,7 @@ public abstract class Money {
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, null);
     }
 
     abstract public Money times(int multiplier);
