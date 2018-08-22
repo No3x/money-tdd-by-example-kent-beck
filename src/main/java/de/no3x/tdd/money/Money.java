c38f8b3bbf5fc8acaaebfd1415aa9ebcc47769b7
Christian Zöller <noex_home@yahoo.de>
2018-08-22 12:00:40 +0200

    Pulled instance method Dollar.equals() up to the class Money.

--- src/main/java/de/no3x/tdd/money/Money.java
+++ src/main/java/de/no3x/tdd/money/Money.java
@@ -2,4 +2,10 @@ package de.no3x.tdd.money;
 
 public class Money {
     protected int amount;
+
+    @Override
+    public boolean equals(Object obj) {
+        Money money = (Money) obj;
+        return amount == money.amount;
+    }
 }
