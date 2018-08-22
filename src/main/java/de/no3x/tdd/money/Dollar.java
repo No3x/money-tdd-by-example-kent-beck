8e5cc92aad726fc00b4f35329bfde352a2cfaa65
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 12:59:33 +0200

    Added a parameter currency to Dollar's constructor as Franc's.

--- src/main/java/de/no3x/tdd/money/Dollar.java
+++ src/main/java/de/no3x/tdd/money/Dollar.java
@@ -2,13 +2,13 @@ package de.no3x.tdd.money;
 
 class Dollar extends Money {
 
-    Dollar(int amount) {
+    Dollar(int amount, String currency) {
         this.amount = amount;
-        currency = "USD";
+        this.currency = currency;
     }
 
     public Money times(int multiplier) {
-        return new Dollar(amount * multiplier);
+        return Money.dollar(amount * multiplier);
     }
 
 }
