760262fe9cbe85ac10d4c902c3499ccbf8eeaaed
Christian Zöller <noex_home@yahoo.de>
2018-08-22 14:39:33 +0200

    Asked the bank for the right rate in the method Money.reduce().

--- src/main/java/de/no3x/tdd/money/Money.java
+++ src/main/java/de/no3x/tdd/money/Money.java
@@ -43,9 +43,7 @@ public class Money implements Expression {
 
     @Override
     public Money reduce(Bank bank, String to) {
-        int rate = (currency.equals("CHF") && to.equals("USD"))
-                ? 2
-                : 1;
+        int rate = bank.rate(currency, to);
         return new Money(amount / rate, to);
     }
 }
