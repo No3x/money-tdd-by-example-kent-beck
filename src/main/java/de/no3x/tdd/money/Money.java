eac422417885b5825dd21a8fe55642951028a428
Christian Zöller <noex_home@yahoo.de>
2018-08-22 13:51:04 +0200

    Money.plus() needs to return an Expression.

--- src/main/java/de/no3x/tdd/money/Money.java
+++ src/main/java/de/no3x/tdd/money/Money.java
@@ -37,7 +37,7 @@ public class Money {
         return amount + " " + currency;
     }
 
-    public Money plus(Money addend) {
+    public Expression plus(Money addend) {
         return new Money(amount + addend.amount, currency);
     }
 }
