17b78313f227ee092fe8bac0956bdcca937f65c0
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 13:17:01 +0200

    Added testDifferentClassEquality().

--- src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
+++ src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
@@ -36,4 +36,10 @@ public class MoneyExampleTest {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
     }
+
+    @Test
+    public void testDifferentClassEquality() {
+        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
+    }
+
 }
