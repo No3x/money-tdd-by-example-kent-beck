fcd64eceaffb95218f90c65d76f1d00814bf114c
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 14:31:40 +0200

    Stubbed method Bank.addRate().

--- src/main/java/de/no3x/tdd/money/Bank.java
+++ src/main/java/de/no3x/tdd/money/Bank.java
@@ -5,4 +5,8 @@ public class Bank {
     public Money reduce(Expression source, String to) {
         return source.reduce(to);
     }
+
+    public void addRate(String from, String to, int rate) {
+
+    }
 }
