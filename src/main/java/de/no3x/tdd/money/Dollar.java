61073ea62224a640920adb61c008ce9812bfd315
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 12:00:40 +0200

    Pulled instance method Dollar.equals() up to the class Money.

--- src/main/java/de/no3x/tdd/money/Dollar.java
+++ src/main/java/de/no3x/tdd/money/Dollar.java
@@ -9,10 +9,4 @@ class Dollar extends Money {
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        Money money = (Money) obj;
-        return amount == money.amount;
-    }
 }
