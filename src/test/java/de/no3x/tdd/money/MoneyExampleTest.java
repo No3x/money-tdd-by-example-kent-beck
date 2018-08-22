129022bc4004db6cf0ec53cdd031a47c7e4d817c
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 13:48:20 +0200

    Updated the testSimpleAddition() to use the expression metaphor. ( + 3 CHF) * 5. A Money is the atomic form of an expression. Operations result in Expressions, one of which will be a Sum. Once the operation (such as adding up the value of a portfolio) is complete, the resulting Expression can be reduced back to a single currency given a set of exchange rates.

--- src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
+++ src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
@@ -28,7 +28,7 @@ public class MoneyExampleTest {
 
     @Test
     public void testSimpleAddition() {
-        Money sum = Money.dollar(5).plus(Money.dollar(5));
-        assertEquals(Money.dollar(10), sum);
+
+        assertEquals(Money.dollar(10), reduced);
     }
 }
