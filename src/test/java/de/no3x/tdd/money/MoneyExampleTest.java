dd5bc6cab736d30908cccda6d66a74e4a61e3866
Christian Zöller <noex_home@yahoo.de>
2018-08-22 13:49:53 +0200

    Got a five-dollar note to finish the testSimpleAddition().

--- src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
+++ src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
@@ -28,7 +28,7 @@ public class MoneyExampleTest {
 
     @Test
     public void testSimpleAddition() {
-
+        Money five = Money.dollar(5);
         Expression sum = five.plus(five);
         Bank bank = new Bank();
         Money reduced = bank.reduce(sum, "USD");
