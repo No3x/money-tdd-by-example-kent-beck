4abfbc368c5d7863aea1495d6eb82d2545e08b28
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 12:55:45 +0200

    The factory method Money.franc() passed actual parameter 'CHF'.

--- src/main/java/de/no3x/tdd/money/Money.java
+++ src/main/java/de/no3x/tdd/money/Money.java
@@ -16,7 +16,7 @@ public abstract class Money {
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount, null);
+        return new Franc(amount, "CHF");
     }
 
     abstract public Money times(int multiplier);
