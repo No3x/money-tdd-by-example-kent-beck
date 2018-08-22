06187bda4a66f3991489718a87d4cd1db33af1d7
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 11:39:22 +0200

    Copied the test code of Dollar and replaced Dollar with Franc.

--- src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
+++ src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
@@ -21,4 +21,11 @@ public class MoneyExampleTest {
         assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
 }
