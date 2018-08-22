37e2cfcaac35b2513e85a709e39280daf67553a0
Christian Zöller <noex_home@yahoo.de>
2018-08-22 15:22:15 +0200

    Made the type of the return value of Money.dollar() to be Expression in testMixedAddition().

--- src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
+++ src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
@@ -74,7 +74,7 @@ public class MoneyExampleTest {
 
     @Test
     public void testMixedAddition() {
-        Money fiveBucks = Money.dollar(5);
+        Expression fiveBucks = Money.dollar(5);
         Expression tenFrancs = Money.franc(10);
         Bank bank = new Bank();
         bank.addRate("CHF", "USD", 2);
