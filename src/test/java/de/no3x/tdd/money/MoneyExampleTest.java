5fe288c3ba7a2b768fd5a112c9bf13c8d86a848e
Christian Zöller <noex_home@yahoo.de>
2018-08-22 13:31:19 +0200

    Deleted duplicated assertions in testEquality().

--- src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
+++ src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
@@ -19,8 +19,6 @@ public class MoneyExampleTest {
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
