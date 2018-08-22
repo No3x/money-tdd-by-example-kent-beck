9d8f09bb68b0abb6e2c9ab097f5c92daddfda9ec
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 14:22:20 +0200

    Eliminated all those ugly casts and class checks in Bank.reduce().

--- src/main/java/de/no3x/tdd/money/Bank.java
+++ src/main/java/de/no3x/tdd/money/Bank.java
@@ -3,8 +3,6 @@ package de.no3x.tdd.money;
 public class Bank {
 
     public Money reduce(Expression source, String to) {
-        if (source instanceof Money) return (Money)source.reduce(to);
-        Sum sum = (Sum) source;
-        return sum.reduce(to);
+        return source.reduce(to);
     }
 }
