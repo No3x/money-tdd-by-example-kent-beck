4b469f7b82646cf5130ac83d347e40a6436629a2
Christian Zöller <noex_home@yahoo.de>
2018-08-22 14:39:04 +0200

    Updated method Money.reduce() and Sum.reduce() to add a Bank as the first parameter.

--- src/main/java/de/no3x/tdd/money/Money.java
+++ src/main/java/de/no3x/tdd/money/Money.java
@@ -42,7 +42,7 @@ public class Money implements Expression {
     }
 
     @Override
-    public Money reduce(String to) {
+    public Money reduce(Bank bank, String to) {
         int rate = (currency.equals("CHF") && to.equals("USD"))
                 ? 2
                 : 1;
