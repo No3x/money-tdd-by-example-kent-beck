ddaf172d492e774223dd4c03ac9432972e04dc08
Christian Zöller <noex_home@yahoo.de>
2018-08-22 16:18:50 +0200

    Added generic types for rates to avoid cast

--- src/main/java/de/no3x/tdd/money/Bank.java
+++ src/main/java/de/no3x/tdd/money/Bank.java
@@ -4,7 +4,7 @@ import java.util.Hashtable;
 
 public class Bank {
 
-    private Hashtable rates = new Hashtable();
+    private Hashtable<Pair, Integer> rates = new Hashtable();
 
     private class Pair {
         private String from;
@@ -33,11 +33,10 @@ public class Bank {
 
     int rate(String from, String to) {
         if (from.equals(to)) return 1;
-        Integer rate = (Integer) rates.get(new Pair(from, to));
-        return rate.intValue();
+        return rates.get(new Pair(from, to));
     }
 
     public void addRate(String from, String to, int rate) {
-        rates.put(new Pair(from, to), new Integer(rate));
+        rates.put(new Pair(from, to), rate);
     }
 }
