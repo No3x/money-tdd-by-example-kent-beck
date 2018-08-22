4c334045f7266206d0560fae312c4e927c536793
Christian Zöller <noex_home@yahoo.de>
2018-08-22 14:39:59 +0200

    Moved the calculation of rate to the method Bank.rate().

--- src/main/java/de/no3x/tdd/money/Bank.java
+++ src/main/java/de/no3x/tdd/money/Bank.java
@@ -6,6 +6,12 @@ public class Bank {
         return source.reduce(this, to);
     }
 
+    int rate(String from, String to) {
+       return (from.equals("CHF") && to.equals("USD"))
+                ? 2
+                : 1;
+    }
+
     public void addRate(String from, String to, int rate) {
 
     }
