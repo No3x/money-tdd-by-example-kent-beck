20de7c56a9b20972f8fa21dca6c695a294e5bb79
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 11:07:31 +0200

    Moved the setting of field Dollar.amount from object initialization to the Dollar.times() method.

--- src/test/java/de/no3x/tdd/money/Dollar.java
+++ src/test/java/de/no3x/tdd/money/Dollar.java
@@ -2,12 +2,13 @@ package de.no3x.tdd.money;
 
 class Dollar {
 
-    int amount = 5 * 2;
+    int amount;
 
     Dollar(int amount) {
     }
 
     void times(int multiplier) {
+        amount = 5 * 2;
     }
 
 }
