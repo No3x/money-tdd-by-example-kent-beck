b57b4ef0b836662010822d40d49a03178cdbbde1
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 12:09:12 +0200

    Added an assertion in testEquality() for comparing Francs and Dollars.

--- src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
+++ src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
@@ -21,6 +21,7 @@ public class MoneyExampleTest {
         assertFalse(new Dollar(5).equals(new Dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 
     @Test
