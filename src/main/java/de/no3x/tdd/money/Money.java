2143a2a7c267f2591e332e1c650c947781f220b9
Christian Zöller <noex_home@yahoo.de>
2018-08-22 15:20:04 +0200

    Changed the type of the argument to Money.plus() and the return value of Money.times() to be Expression.

--- src/main/java/de/no3x/tdd/money/Money.java
+++ src/main/java/de/no3x/tdd/money/Money.java
@@ -24,7 +24,7 @@ public class Money implements Expression {
         return new Money(amount, "CHF");
     }
 
-    public Money times(int multiplier) {
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 
@@ -37,7 +37,7 @@ public class Money implements Expression {
         return amount + " " + currency;
     }
 
-    public Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
