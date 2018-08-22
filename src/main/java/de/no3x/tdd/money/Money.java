baa7c39dbfa4b663ea825fdf2867142658951ce0
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 12:58:03 +0200

    Added an actual parameter 'USD' in the caller of Dollar's constructor in the factory method Money.dollar().

--- src/main/java/de/no3x/tdd/money/Money.java
+++ src/main/java/de/no3x/tdd/money/Money.java
@@ -12,7 +12,7 @@ public abstract class Money {
     }
 
     public static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     public static Money franc(int amount) {
