03d938a358ee674d8fef4cf6c1905621be64e25b
Christian Zöller <No3x@users.noreply.github.com>
2018-08-22 14:25:43 +0200

    Added testReduceMoneyDifferentCurrency() for having 2 francs and wanting a dollar.

--- src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
+++ src/test/java/de/no3x/tdd/money/MoneyExampleTest.java
@@ -58,4 +58,12 @@ public class MoneyExampleTest {
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
 }
