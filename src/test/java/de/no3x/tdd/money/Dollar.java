c1502059c157d5b3df065f24e2dd315b064721fa
Christian Zöller <noex_home@yahoo.de>
2018-08-22 11:08:15 +0200

    Eliminated the duplicated value 5 from the production code.

--- src/test/java/de/no3x/tdd/money/Dollar.java
+++ src/test/java/de/no3x/tdd/money/Dollar.java
@@ -5,10 +5,11 @@ class Dollar {
     int amount;
 
     Dollar(int amount) {
+        this.amount = amount;
     }
 
     void times(int multiplier) {
-        amount = 5 * 2;
+        amount = amount * 2;
     }
 
 }
